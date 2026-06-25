12904fc9ede06f25e945008867ced16b3d4ca0e6
seroga <sergijvchichur@gmail.com>
2026-06-25 19:14:54 +0300

    renamed name of the interface in task-3 and changed type of status in task-5

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,8 +1,6 @@
 type Status = "loading" | "success" | "error";
 
-let status: Status;
-
-function logStatus(status: string): void {
+function logStatus(status: Status): void {
   if (status === "loading") {
     console.log("Loading...");
   } else if (status === "success") {
