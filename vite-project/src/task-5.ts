ffbea63b4663d05c735541ed4fe3a561fe97fc34
seroga <sergijvchichur@gmail.com>
2026-06-25 18:40:27 +0300

    added vite and axios and made tasks 1-8

---
+++ vite-project/src/task-5.ts
@@ -0,0 +1,15 @@
+type Status = "loading" | "success" | "error";
+
+let status: Status;
+
+function logStatus(status: string): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
